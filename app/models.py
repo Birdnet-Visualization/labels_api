1778e45a3bc267458231d147ccfa941dabda7c52
Victor Vargas <victorvargassandoval93@gmail.com>
2023-11-06 23:40:16 -0300

    Timestamp label added

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from mongoengine import Document, FloatField, StringField, IntField
 
 class Label(Document):
     label_id = IntField(required=True)
+    start_timestamp = IntField(required=True)
+    end_timestamp = IntField(required=True)
     start = FloatField(required=True)
     end = FloatField(required=True)
     scientific_name = StringField(required=True)
@@ -12,6 +14,8 @@ class Label(Document):
     def to_dict(self):
         return {
             "label_id": self.label_id,
+            "start_timestamp": self.start_timestamp,
+            "end_timestamp": self.end_timestamp,
             "start": self.start,
             "end": self.end,
             "scientific_name": self.scientific_name,
